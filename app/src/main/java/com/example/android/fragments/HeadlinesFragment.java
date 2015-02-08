62ee5884195c5655d4de1014a6b1517d71f15377
netskink <davisjf@gmail.com>
2015-02-08 15:10:10 -0700

    Added notes from the text whle I was following along.

--- app/src/main/java/com/example/android/fragments/HeadlinesFragment.java
+++ app/src/main/java/com/example/android/fragments/HeadlinesFragment.java
@@ -23,9 +23,52 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+
+/*
+COMMUNICATING WITH OTHER FRAGMENTS
+
+In order to reuse the Fragment UI components, you should build each as a completely
+self-contained, modular component that defines its own layout and behaviour.  once you have
+defined these reusable Fragments, you can associate them with the application logic to realize
+the overall composite UI.
+
+Often you will want one fragment to communicate with another, for example
+to change the content based on a user event.  All fragment-to-fragment communication is done
+through the associated activity.  Two fragments should never communicate directly.
+
+
+DEFINE AND INTERFACE
+
+To allow a Fragment to communicate up to its activity, you can define an interface in the Fragment
+class and implement it within the Activity.  The fragment captures the interface implementation during
+its onAttach() lifecycle method and can then call the Interface methods in order to communicate with
+the Activity.
+
+
+Here is an example of Fragment to Activity communication:
+1. The Fragment implements OnCLASSNAMESelectedListener
+2. In onAttach() make sure the container activity has
+   implemented the callback interface.
+
+Now, the fragment can deliver messages to the activity by calling
+the onArticleSelected() method (or other methods in the interface)
+using mCallback instance of the OnHeadlineSelectedListener interface.
+
+For example, see onListItemClick below which is called when a user
+clicks on a list item in the fragment.  The fragment uses the callback
+interface to deliver the event to the parent activity.
+
+<See the MainActivity for the interface implementation and how to handle
+the Fragment to Activity communication in the activity.
+ */
+
+
+
 public class HeadlinesFragment extends ListFragment {
     OnHeadlineSelectedListener mCallback;
 
+    // This is the first part required to implement
+    // Fragment to Activity communication.
     // The container Activity must implement this interface so the frag can deliver messages
     public interface OnHeadlineSelectedListener {
         /** Called by HeadlinesFragment when a list item is selected */
@@ -56,6 +99,8 @@ public class HeadlinesFragment extends ListFragment {
     }
 
     @Override
+    // This is the second part of the example on how to
+    // implement Fragment to Activity communication:
     public void onAttach(Activity activity) {
         super.onAttach(activity);
 
@@ -69,6 +114,10 @@ public class HeadlinesFragment extends ListFragment {
         }
     }
 
+    /*
+    This shows how the fragment can deliver the event to the parent activity
+    establishing fragment to activity communication.
+     */
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         // Notify the parent activity of selected item
--- app/src/main/java/com/example/android/fragments/MainActivity.java
+++ app/src/main/java/com/example/android/fragments/MainActivity.java
@@ -19,12 +19,43 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 
+
+/* in order to handle fragment to activity communication, the activity must implement
+a listener as shown below in the implements clause.
+
+
+DELIVER A MESSAGE TO A FRAGMENT
+
+The host activity can deliver messages to a fragment by capturing the Fragment
+instance with findFragmentById(), then directly call the fragment's public methods.
+
+For instance, imaging that the activity shown above may contain another fragment that's
+used to display the item specified by the data returned in the above callback method.
+In this case, the activity can pass the information
+received in the callback method to the other fragment that will display the item:
+
+<see onArticleSelected() routine>
+
+ */
 public class MainActivity extends FragmentActivity 
         implements HeadlinesFragment.OnHeadlineSelectedListener {
 
     /** Called when the activity is first created. */
+
+    // Inside your activity, call getSupportFragmentManager() to
+    // get a FragmentManager using the support Library API's.  Then
+    // call beginTransaction() to create a FragmentTransaction and call
+    // add() to add a fragment.
+
+    // You can perform multiple fragment transaction for the activity using the same
+    // FragmentTransaction.  When you're ready to make the changes, you must call commit().
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
+        // These two lines are all that is necessary to add
+        // a fragment to an activity.  Depending upon screen
+        // size it adds either the normal or large version
+        // of news_articles layout.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_articles);
 
@@ -47,11 +78,19 @@ public class MainActivity extends FragmentActivity
             firstFragment.setArguments(getIntent().getExtras());
 
             // Add the fragment to the 'fragment_container' FrameLayout
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_container, firstFragment).commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, firstFragment).commit();
         }
     }
 
+    /*
+    In order to receive callbacks from the fragment, the activity that hosts the fragment must
+    implement the interface defined in the fragment class.
+
+    For example, the following activity implements the interface from the above example.
+
+    Remember the headlines fragment calls this callback and then it in turn calls down
+    to the article fragment to change its content.
+     */
     public void onArticleSelected(int position) {
         // The user selected the headline of an article from the HeadlinesFragment
 
@@ -66,6 +105,17 @@ public class MainActivity extends FragmentActivity
             articleFrag.updateArticleView(position);
 
         } else {
+            // This is an example of replcing one fragment with another.
+            // Its similar procedure, but you call replace() instead of add().
+            // Keep in mind that when you perform fragment transactions such as replace
+            // or remove one, its often appropriate the allow the user to navigate
+            // backwards and undo the change.  To allow the user to navigate backwards
+            // through fragment transactions, you must call addToBackStack() before you commit
+            // the FragmentTransaction.
+
+
+
+
             // If the frag is not available, we're in the one-pane layout and must swap frags...
 
             // Create fragment and give it an argument for the selected article
@@ -78,6 +128,9 @@ public class MainActivity extends FragmentActivity
             // Replace whatever is in the fragment_container view with this fragment,
             // and add the transaction to the back stack so the user can navigate back
             transaction.replace(R.id.fragment_container, newFragment);
+            // The addToBackStack() method takes an optional string parameter that specifies
+            // aunique name for the transaction.  The name isn't needed unless you plan to perform
+            // advanced fragment operations using the FragmentManager.BackStackEntry API's.
             transaction.addToBackStack(null);
 
             // Commit the transaction
